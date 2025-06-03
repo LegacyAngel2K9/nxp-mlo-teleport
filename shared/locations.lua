c78381ba6f5ce6080e699513d99f38b1475918f0
Angel Knutsen Aune <djangel@live.no>
2025-06-03 04:27:00 +0200

    2.0.0

--- shared/locations.lua
+++ shared/locations.lua
@@ -1,60 +1,125 @@
 Locations = {
-    ["Police"] = {
+    ["Police Stations"] = {
         {
-            name = "Mission Row PD",
-            coords = vector3(440.84, -983.14, 30.69),
-            heading = 90.0,
-            blip = {
-                enabled = true,
-                sprite = 60,
-                color = 38,
-                scale = 0.8
-            },
-            marker = true,
-            permission = "teleport.police"
+            name = "U.S Marshal HQ",
+            description = "Lobby, Meeting Room, Press Conferance, Offices, Garage, Gym, Cells, Toilets.",
+            coords = vector3(-835.25, -694.38, 27.31),
+            heading = 268.05,
+            icon = "fa-building-shield",
+            permission = nil
         },
         {
-            name = "Paleto Bay PD",
-            coords = vector3(-447.93, 6013.38, 31.72),
-            heading = 45.0,
-            blip = {
-                enabled = true,
-                sprite = 60,
-                color = 38,
-                scale = 0.8
-            },
-            marker = true,
-            permission = "teleport.police"
-        }
-    },
-    ["Medical"] = {
+            name = "Paleto PD",
+            description = "Large Garage, Offices, Evidence Room, Shooting Range and More.",
+            coords = vector3(-435.62, 6023.05, 31.49),
+            heading = 132.38,
+            icon = "fa-building-shield",
+            permission = nil
+        },
         {
-            name = "Pillbox Hospital",
-            coords = vector3(307.29, -595.46, 43.28),
-            heading = 340.0,
-            blip = {
-                enabled = true,
-                sprite = 61,
-                color = 2,
-                scale = 0.8
-            },
-            marker = true,
-            permission = "teleport.ems"
+            name = "Sandy PD & Impound",
+            description = "Large Garage, Offices, Evidence Room, Shooting Range and More.",
+            coords = vector3(1814.4, 3665.08, 34.8),
+            heading = 297.2,
+            icon = "fa-building-shield",
+            permission = nil
+        },
+        {
+            name = "Weazel PD",
+            description = "Garage, Open Offices, Spacious Hallways, Shooting Range, Lab and More.",
+            coords = vector3(-602.2, -929.94, 23.86),
+            heading = 268.34,
+            icon = "fa-building-shield",
+            permission = nil
         }
     },
+
+    ["Fire Departments"] = {
+        {
+            name = "Davis Fire Dept",
+            description = "6 Car Bay Area, Offices, Gym, Bedrooms, Kitchen and More.",
+            coords = vector3(225.91, -1640.56, 29.28),
+            heading = 137.61,
+            icon = "fa-fire-extinguisher",
+            permission = nil
+        },
+        {
+            name = "Paleto Fire",
+            description = "Garage, Offices, Small Outside Garage for Boats, Working Fire Police and Warning Lights and More.",
+            coords = vector3(-380.93, 6139.42, 31.25),
+            heading = 220.36,
+            icon = "fa-fire-extinguisher",
+            permission = nil
+        },
+        {
+            name = "El Burro Fire Dept",
+            description = "Reception, Toilets, Press Conferance, Offices, Garage with 11 Spots, Gym, Changing Room and More.",
+            coords = vector3(1211.7, -1451.67, 34.87),
+            heading = 174.91,
+            icon = "fa-fire-extinguisher",
+            permission = nil
+        }
+    },
+
+    ["EMS"] = {
+        {
+            name = "Paleto Hospital",
+            description = "Helipad, Offices, Doctor Rooms, Surgery Room, MRI, X-Ray Room, Diagnostics Room and More.",
+            coords = vector3(-238.64, 6308.9, 31.48),
+            heading = 42.17,
+            icon = "fa-hospital",
+            permission = nil
+        },
+        {
+            name = "Ambu Central",
+            description = "Garage, Standby Room, Meeting Room, Office, Bedrooms and More.",
+            coords = vector3(-1271.54, -1359.23, 4.3),
+            heading = 107.14,
+            icon = "fa-hospital",
+            permission = nil
+        }
+    },
+
     ["Misc"] = {
         {
-            name = "Training Ground",
-            coords = vector3(1985.41, 3050.10, 47.22),
-            heading = 180.0,
-            blip = {
-                enabled = true,
-                sprite = 309,
-                color = 5,
-                scale = 0.8
-            },
-            marker = true,
-            permission = false -- no permission required
+            name = "Liquor Store",
+            description = "Store Area, Storage Room, Locker Room, Toilets, Boss Office and More.",
+            coords = vector3(-61.86, -93.29, 57.77),
+            heading = 293.22,
+            icon = "fa-martini-glass",
+            permission = nil
+        },
+        {
+            name = "24/7 Store",
+            description = "Store Area, Storage Room, Locker Room, Toilets and More.",
+            coords = vector3(80.02, -217.3, 54.64),
+            heading = 157.4,
+            icon = "fa-store",
+            permission = nil
+        },
+        {
+            name = "Paleto HUB",
+            description = "Car Repair, Offices, Toilets, Meeting Room and More.",
+            coords = vector3(74.7, 6564.08, 31.75),
+            heading = 176.1,
+            icon = "fa-car",
+            permission = nil
+        },
+        {
+            name = "Ring Of Fire",
+            description = "Dining Area, Toilets, Kitchen, Storage Area, Office, Locker Room and More.",
+            coords = vector3(153.52, -1431.99, 29.25),
+            heading = 227.84,
+            icon = "fa-utensils",
+            permission = nil
+        },
+        {
+            name = "Neko Nightclub",
+            description = "Music, NPC's, Dancing Floor, VIP Area, Stripclub, Animated Lights and More.",
+            coords = vector3(372.99, 257.98, 102.86),
+            heading = 157.45,
+            icon = "fa-champagne-glasses",
+            permission = nil
         }
     }
 }
